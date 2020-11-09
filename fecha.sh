1ed5f122fcf5cfc9a1c64f312ecc8133ab3d40ef
DiegoGalnares <72581751+DiegoGalnares@users.noreply.github.com>
2020-11-09 01:36:47 -0600

    Add files via upload

--- fecha.sh
+++ fecha.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-echo -e -n "\e[33mFecha actual :"
-head -2 /proc/driver/rtc | cut -d ':' -f 2 | tail -1
-
-echo -e -n "\e[32mHora actual :"
-head -2 /proc/driver/rtc | cut -d ':' -f 2- | head -1
-
+echo "La fecha actual es: ";command date +%d-%B-%Y
